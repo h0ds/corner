49db188240c4e7a89d29f3e63f041509e32eb267
hodson <gregjworrall@gmail.com>
2024-11-04 16:09:41 +0000

    Corner connection

--- src/components/preferences/Connections.tsx
+++ src/components/preferences/Connections.tsx
@@ -15,20 +15,18 @@ export const Connections: React.FC = () => {
         </Button>
       </div> */}
 
-      {/* Lex Connection */}
+      {/* Corner Connection */}
       <div className="flex items-center justify-between p-3 rounded-sm bg-muted">
         <div className="flex items-center gap-3">
           <div className="p-2 bg-background rounded-sm">
-            <svg width="20" height="20" viewBox="0 0 237 237" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M1 21C1 9.9543 9.9543 1 21 1H216C227.046 1 236 9.9543 236 21V216C236 227.046 227.046 236 216 236H21C9.9543 236 1 227.046 1 216V21Z" fill="black"/>
-              <path d="M57.6753 75.28V151H68.4753V160H47.4753V75.28H57.6753Z" fill="white"/>
-              <path d="M104.062 97.36C122.182 97.36 132.742 111.76 132.142 132.28H85.9419C86.9019 146.44 94.9419 152.44 105.982 152.44C114.982 152.44 118.822 147.88 121.342 140.2L131.182 143.68C127.222 154.24 120.142 161.44 105.742 161.44C85.9419 161.44 75.7419 148.36 75.7419 129.16C75.7419 111.76 87.1419 97.36 104.062 97.36ZM104.062 106.36C95.1819 106.36 88.1019 112.84 86.3019 123.64H121.582C119.902 112.6 113.422 106.36 104.062 106.36Z" fill="white"/>
-              <path d="M173.655 126.64L197.775 160H184.575L167.295 134.32L150.015 160H137.775L160.935 127.24L139.935 98.8H153.015L167.295 119.68L182.055 98.8H194.295L173.655 126.64Z" fill="white"/>
-              <path d="M21 0.5C9.67816 0.5 0.5 9.67816 0.5 21V216C0.5 227.322 9.67816 236.5 21 236.5H216C227.322 236.5 236.5 227.322 236.5 216V21C236.5 9.67816 227.322 0.5 216 0.5H21Z" stroke="white"/>
-            </svg>
+          <svg width="20" height="20" viewBox="0 0 240 240" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect width="240" height="240" rx="22" fill="black"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M180 60H60V180L180 60Z" fill="white"/>
+</svg>
+
           </div>
           <div>
-            <h4 className="text-sm font-medium">Lex</h4>
+            <h4 className="text-sm font-medium">Corner</h4>
             <p className="text-xs text-muted-foreground">Streamline usage across providers</p>
           </div>
         </div>
